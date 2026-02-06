891548dd2507acdd641da523c98e2e155475eb1e
grib24 <gribdmitrii0@gmail.com>
2026-02-07 02:51:34 +0300

    Update orange_ussi_arm64.mk

--- orange_ussi_arm64.mk
+++ orange_ussi_arm64.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/recovery/config/common.mk)
+# $(call inherit-product, vendor/recovery/config/common.mk)
 
 # Inherit from ussi_arm64 device
 $(call inherit-product, device/realme/ussi_arm64/device.mk)
