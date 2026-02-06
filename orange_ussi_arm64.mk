76dfc6b88ec608b5d0a372b53b43883927722574
grib24 <gribdmitrii0@gmail.com>
2026-02-06 21:44:06 +0300

    Update and rename twrp_ussi_arm64.mk to orange_ussi_arm64.mk

--- orange_ussi_arm64.mk
+++ orange_ussi_arm64.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/fox/config/common.mk)
 
 # Inherit from ussi_arm64 device
 $(call inherit-product, device/realme/ussi_arm64/device.mk)
 
 PRODUCT_DEVICE := ussi_arm64
-PRODUCT_NAME := twrp_ussi_arm64
+PRODUCT_NAME := orange_ussi_arm64
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := ussi_arm64_full
 PRODUCT_MANUFACTURER := realme
