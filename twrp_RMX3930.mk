8dd04a95bfae0a839923aa3fb25ca4999fed164e
grib24 <gribdmitrii0@gmail.com>
2026-02-09 01:33:30 +0300

    Update twrp_RMX3930.mk

--- twrp_RMX3930.mk
+++ twrp_RMX3930.mk
@@ -11,5 +11,5 @@ $(call inherit-product, device/realme/RMX3930/device.mk)
 PRODUCT_DEVICE := RMX3930
 PRODUCT_NAME := twrp_RMX3930
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme C61
+PRODUCT_MODEL := RealmeC61
 PRODUCT_MANUFACTURER := realme
