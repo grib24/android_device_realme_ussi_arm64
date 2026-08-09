9535a2e00b3f3fa80be7e8caf88d57a52378af30
grib24 <gribdmitrii0@gmail.com>
2026-08-09 21:58:49 +0300

    Rename configs to exact latte board name for builder compliance

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,2 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_ussi_arm64.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_latte-userdebug \
-    twrp_latte-eng
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_latte.mk 
+COMMON_LUNCH_CHOICES := twrp_latte-userdebug twrp_latte-eng 
