985e170e3bf8d56865b5b0860feebb0553c21e8d
grib24 <gribdmitrii0@gmail.com>
2026-08-12 08:29:36 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,17 +1,10 @@
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-# Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+# Inherit from TWRP base configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl.recovery \
-    fastbootd
-
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Copying fixed latte fstab to first stage ramdisk
+# Device specific configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.ums9230_latte:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.ums9230_latte
 
