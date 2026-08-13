61f5eff29d16a6f85239495dc7cd78e83c0a3bce
grib24 <gribdmitrii0@gmail.com>
2026-08-13 19:42:27 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,9 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_latte.mk 
-COMMON_LUNCH_CHOICES := twrp_latte-userdebug twrp_latte-eng 
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_latte.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_latte-eng \
+    twrp_latte-userdebug \
+    twrp_latte-user
